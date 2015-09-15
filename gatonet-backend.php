be1b14155a30b97ccf6978199f17e79737fedd77
G470 <info@gatonet.de>
2015-09-16 00:39:24 +0200

    Added version number removal

--- gatonet-backend.php
+++ gatonet-backend.php
@@ -207,7 +207,18 @@ class GatonetCustomizer {
     }
 
 
-
+    /**
+     *  REMOVE VERSION NUMBERS
+     *  Remove ?ver=x.x from css and js good idea in general...
+     
+     */
+	function remove_cssjs_ver( $src ) {
+	 if( strpos( $src, '?ver=' ) )
+	 $src = remove_query_arg( 'ver', $src );
+	 return $src;
+	}
+	add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+	add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
 
 
 
